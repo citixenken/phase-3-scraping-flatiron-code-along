93a3b5e9beb23fd9df3599f404f349c0f1a05716
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-08-29 08:25:34 +0300

    commit tree 362f336b14f35149c91629b5301916a8264f626c

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,28 @@
+# require 'nokogiri'
+# require 'open-uri'
+# require 'awesome_print'
 
+# html = URI.open('http://learn-co-curriculum.github.io/site-for-scraping/courses')
+# doc = Nokogiri::HTML(html)
+# courses = doc.css('article.post > h2')
+
+# ap courses
+
+class Course
+    attr_accessor :title, :schedule, :description
+    
+    @@all = []
+
+    def initialize
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.reset_all
+        @@all.clear
+    end
+end
 
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,48 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
+  def get_page
+    html = URI.open('http://learn-co-curriculum.github.io/site-for-scraping/courses')
+    Nokogiri::HTML(html)
+
+    # binding.pry test data
+    # =====================
+    # doc = Nokogiri::HTML(html)
+    # doc.css('article.post').first.css('h2').text => title
+    # doc.css('article.post').first.css('em.date').text => schedule
+    # doc.css('article.post').first.css('p').text => description
+
+    # doc.css('article.post').collect do |post|
+    #   course = Course.new
+    #   course.title = post.css('h2').text
+    #   course.schedule = post.css('em.date').text
+    #   course.description = post.css('p').text
+    # end
+    # binding.pry
+  end
+
+  def get_courses
+    self.get_page.css('article.post')
+  end
+
+  def make_courses
+    self.get_courses.collect do |post|
+        course = Course.new
+        course.title = post.css('h2').text
+        course.schedule = post.css('em.date').text
+        course.description = post.css('p').text
+      end
+  end
   
   def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title && course.title != ""
-        puts "Title: #{course.title}"
+        puts "  Title: #{course.title}"
         puts "  Schedule: #{course.schedule}"
         puts "  Description: #{course.description}"
       end
@@ -18,5 +51,6 @@ class Scraper
   
 end
 
+Scraper.new.get_page
 
 
